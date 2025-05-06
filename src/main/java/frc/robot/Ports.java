775128a300ba56bd2403d7817c9789d72cdc774a
ishitarajan <ishitarajan21@gmail.com>
2025-05-06 16:51:25 -0500

    06/05/2025
    Drive code

---
+++ src/main/java/frc/robot/Ports.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+public class Ports {
+    //speed sparkmax
+    public static final int backLeftDrive = 8;
+    public static final int frontLeftDrive = 2;
+    public static final int backRightDrive = 6;
+    public static final int frontRightDrive = 4;
+
+    //angle sparkmax
+    public static final int backLeftSteer = 7;
+    public static final int frontLeftSteer = 1;
+    public static final int backRightSteer = 5;
+    public static final int frontRightSteer = 3;
+    
+}
